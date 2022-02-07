200060bd31977bcf94a146d45f6364bf417f6a7e
it21178504 <99179062+it21178504@users.noreply.github.com>
2022-02-07 11:42:39 +0000

    tutorial 1

--- Tute04.c
+++ Tute04.c
@@ -20,7 +20,7 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
-   
+}
    int minimum(int n1, int n2){
      if(n1<n2)
       return n1;
@@ -36,4 +36,4 @@ int main() {
    int multiply(int n1, int n2){
      return n1*n2;
    }
-}
+
